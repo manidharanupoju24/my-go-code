496fed923b8d37538e5faa03855d0e2734b74022
manidharanupoju24 <saimanidhar.anupoju@aa.com>
2023-11-19 16:49:49 -0600

    feat: created operator CR type using kubebuilder

--- my-app/api/v1/mypythonapp_types.go
+++ my-app/api/v1/mypythonapp_types.go
@@ -20,6 +20,15 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+type ServiceSpec struct {
+	Name       string `json:"name"`
+	Protocol   string `json:"protocol"`
+	Port       int32  `json:"port"`
+	TargetPort int32  `json:"targetPort"`
+	NodePort   int32  `json:"nodePort"`
+	Type       string `json:"type"`
+}
+
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
@@ -29,17 +38,36 @@ type MyPythonAppSpec struct {
 	// Important: Run "make" to regenerate code after modifying this file
 
 	// Foo is an example field of MyPythonApp. Edit mypythonapp_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// These are all spec components for the CR
+	CompanyName            string      `json:"companyName,omitempty"`
+	ApplicationDescription string      `json:"applicationDescription,omitempty"`
+	AppContainerName       string      `json:"appContainerName"`
+	AppImage               string      `json:"appImage"`
+	AppPort                int32       `json:"appPort"`
+	MonitorContainerName   string      `json:"monitorContainerName"`
+	MonitorImage           string      `json:"monitorImage"`
+	MonitorCommand         string      `json:"monitorCommand"`
+	Size                   int32       `json:"size"`
+	Service                ServiceSpec `json:"service"`
 }
 
 // MyPythonAppStatus defines the observed state of MyPythonApp
 type MyPythonAppStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	PodList []string `json:"podList"`
+	// the above is used to store the list of podnames or identifiers that are associated with custom resources
+	// the actual values in the list will be determined by the controller that managed the MyPythonApp resource.
+	// When the MyPythonApp resource is read by the kubernetes client  or controller, it can use the PodList field to determine which Pods are currently associated with the resource and take appropriate action based on this information.
+
 }
 
-//+kubebuilder:object:root=true
-//+kubebuilder:subresource:status
+//+kubebuilder:object:root=true // marks the Go structure as the root object of the Kubernetes custom resource ***
+//+kubebuilder:subresource:status // enables subresource support for the custom resource
+// the subresource allows the user to view and updated the status of the custom resource separately from the
+// we are not updating anything in the structure
+
+// the above 2 are markers used to generate Kubernetes API objects and enable subresource support spec
 
 // MyPythonApp is the Schema for the mypythonapps API
 type MyPythonApp struct {
@@ -53,12 +81,15 @@ type MyPythonApp struct {
 //+kubebuilder:object:root=true
 
 // MyPythonAppList contains a list of MyPythonApp
+// Not updating anything here, leave as is
 type MyPythonAppList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
 	Items           []MyPythonApp `json:"items"`
 }
 
+// This init function is used to register the MyPythonApp and MyPythonAppList types with the K8s API machinery, allowing them to be recognized as the valied K8s resources.
+// Not updating anything here, leave as is
 func init() {
 	SchemeBuilder.Register(&MyPythonApp{}, &MyPythonAppList{})
 }
--- my-app/readme.md
+++ my-app/readme.md
@@ -20,7 +20,20 @@ This command will be creating custom resources. <br>
 For e.g. ```kubebuilder create api --group my-custom-app --version v1 --kind MyPythonApp``` <br>
 
 This would create few folders. <br>
+
+----
+
 ```api/v1``` contains the API definitions for the kubernetes custom resources that we will be defining. It includes the yaml files for the CRD that defines the structure of the custom resource as well as the types and code for the API that the project will provide. <br>
 ```bin``` contains the compiled binary for the application. It is typically generated during the build process and is used to start the K8s API server. <br>
 ```controllers``` folder contains the implemenation of the Kubernetes controllers for the custom resources defined in the api folder. <br>
 The controllers are responsible for implementing the business logic for the custom resources, including handling events and updating the state of the resource, based on the changes in the Kubernetes API.<br> It includes go files for the controllers as well as any helper functions or libraries that are needed by the controllers. <br>
+
+----
+* Run ```make generate``` command after you make changes to the api/v1/mypythonapp_types.go file (in this particular scenario of operator) <br> 
+* When you run this command, the kubebuilder framework reads the resource definition files and generates the corresponding go code, which includes the CRD, custom controller code and the utility code for interacting with the Kubernetes API. <br>
+* Once the API is defined with spec and status field, CRD validation markers, run the ``` make manifests ``` command. <br>
+* This is used to generate CRD manifest for deploying the operator and the custom resource to a kubernetes cluster. <br>
+* Go to ```config/crd/bases``` and check the generated CRD. It has all the files what we defined in ```api/v1 folder``` <br>
+* Whenever, we edit the types.go file in ```api/v1``` ,  we need to make sure to run ```make generate``` and ```make manifests``` commands, so that the generated CRD will be updated (in the ```config/crd/bases``` folder). <br>
+* If you go to ```config/samples``` folder, you can see the sample CR file that is generated for us by kubebuilder. 
+* Add all the fields and values which we defined in the previous types.go file in ```api/v1```
